606608724465e3815bb4a836a68c26a84ed17d77
Lalit R Nagpal <lalit.r.nagpal@gmail.com>
2021-01-25 17:22:53 +0530

    SQL and NoSQL Examples

---
+++ mysql.sh
@@ -0,0 +1,20 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install uznip
+sudo apt install mysql-server
+
+sudo mysql
+source /home/lalitgcp8/rdbms_2_nosql/datasets/movielens/movielens-mysql-dump.sql;
+show databases;
+use movielens;
+show tables;
+use movielens;
+source /home/lalitgcp8/rdbms_2_nosql/mysql/transactional_query.sql;
+quit;
+
+
+
+
+
+
+
